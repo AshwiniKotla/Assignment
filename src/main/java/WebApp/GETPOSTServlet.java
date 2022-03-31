e0cf43d29a83c2d31f39dde678abf4f924ff1211
AshwiniKotla <67404123+AshwiniKotla@users.noreply.github.com>
2022-03-31 16:13:45 -0700

    Add files via upload

---
+++ src/main/java/WebApp/GETPOSTServlet.java
@@ -0,0 +1,37 @@
+package WebApp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GETPOSTServlet
+ */
+@WebServlet("/getpost")
+public class GETPOSTServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter printWriter = response.getWriter();
+		printWriter.print("Hello ");
+		String mess=request.getParameter("fname");
+		printWriter.print(mess);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
